ffd1691f2543529cfe0dfb582da8043d46c055e5
msjahid <nirjonjahid@gmail.com>
2021-06-17 17:01:22 +0600

    viewset & routers

---
+++ viewsetsAndRouters/viewset/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Student, Subject, Teacher, Review
+
+admin.site.register(Subject)
+
+
+@admin.register(Student)
+class StudentInfoAdmin(admin.ModelAdmin):
+
+    list_display = ['student_id', 'student_name', 'subject_list', 'address']
+
+
+@admin.register(Teacher)
+class TeacherInfoAdmin(admin.ModelAdmin):
+
+    list_display = ['teacher_id', 'teacher_name', 'type', 'teacher_subject', 'contact']
+
+
+@admin.register(Review)
+class ReviewInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rating', 'description', 'teacher_ids', 'active', 'created']
