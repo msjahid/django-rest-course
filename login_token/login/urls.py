45c8d22f5080145ec5941c9ba5a652f81ba72947
msjahid <nirjonjahid@gmail.com>
2021-07-02 20:28:44 +0600

    login_token

---
+++ login_token/login/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (StudentListAV, StudentDetailAV, SubjectListAV, SubjectDetailsAV,
+                    TeacherListAV, ReviewList, ReviewDetail, ReviewCreate, StudentList)
+
+
+router = DefaultRouter()
+router.register('student', StudentList, basename='student-list')
+router.register('teacher', TeacherListAV, basename='teacher-list')
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+
+    # path('student/', StudentListAV.as_view(), name='list_student'),
+    # path('student/<int:pk>/', StudentDetailAV.as_view(), name='studentinfo-detail'),
+
+
+    path('subject/', SubjectListAV.as_view(), name='list_subject'),
+    path('subject/<int:pk>/', SubjectDetailsAV.as_view(), name='subjectlist-detail'),
+
+    # path('teacher/', TeacherListAV.as_view(), name='list_teacher'),
+    # path('teacher/<int:pk>/', TeacherDetailsAV.as_view(), name='teacherlist-detail'),
+
+    # path('review/', ReviewList.as_view(), name='list_review'),
+    # path('review/<int:pk>/', ReviewDetail.as_view(), name='reviewlist-detail'),
+
+    path('teacher/<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('teacher/<int:pk>/review/', ReviewList.as_view(), name='list_review'),
+    path('teacher/review/<int:pk>/', ReviewDetail.as_view(), name='reviewlist-detail'),
+]
---
+++ login_token/user_app/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
